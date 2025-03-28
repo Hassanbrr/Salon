72901ceee909e1ea8956a004f2eac602a853adf5
Hassan_b <hasanbahmandar5@gmail.com>
2025-03-29 01:48:14 +0800

    .

--- Areas/Admin/Controllers/ServiceController.cs
+++ Areas/Admin/Controllers/ServiceController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Base;
 using DataAccess.Context;
 using Domain.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -10,7 +11,7 @@ namespace Salon.Areas.Admin.Controllers
     public class ServiceController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-
+        private readonly IWebHostEnvironment _webHostEnvironment;
         public ServiceController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -36,6 +37,64 @@ namespace Salon.Areas.Admin.Controllers
             };
 
             return View(serviceList);
+        }
+        [HttpPost]
+        public IActionResult Create(ServiceVm obj, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null)
+                    {
+                        string wwwRootPath = _webHostEnvironment.WebRootPath;
+                        //finaly name
+                        string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                        //finaly masir
+                        string productPath = Path.Combine(wwwRootPath, @"Images\Service");
+
+                        if (!string.IsNullOrEmpty(obj.Services.ImageUrl))
+                        {
+                            //delete old image
+                            var OldImagePath = Path.Combine(wwwRootPath, obj.Services.ImageUrl.TrimStart('\\'));
+
+                            if (System.IO.File.Exists(OldImagePath))
+                            {
+                                System.IO.File.Delete(OldImagePath);
+                            }
+
+                        }
+
+                        //uplode new image
+                        using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                        {
+                            file.CopyTo(fileStream);
+                        }
+
+                        obj.Services.ImageUrl = @"\Images\Service\" + fileName;
+                    }
+                }
+
+                _unitOfWork.Service.Create(obj.Services);
+                _unitOfWork.SaveChangesAsync();
+                TempData["success"] = "Product Created Successfully";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ServiceVm serviceList = new()
+                {
+
+                    CategoryList = _unitOfWork.Category.FindAll().Select(u => new SelectListItem
+                    {
+                        Text = u.CategoryName,
+                        Value = u.CategoryId.ToString()
+                    }),
+                    Services = new Service()
+                };
+                return View(serviceList);
+            }
         }
     }
 }
