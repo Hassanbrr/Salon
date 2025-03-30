62cfe177db7d742cbe34d86b14c9ac55b73e609b
Hassan_b <hasanbahmandar5@gmail.com>
2025-03-30 21:41:33 +0330

    .

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,84 @@
+﻿using DataAccess.Base;
+using DataAccess.Context;
+using Domain.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Salon.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork )
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categoriesList = await _unitOfWork.Category.FindAll(includeProperties:"ParentCategory").ToListAsync();
+
+            return View(categoriesList);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create(int? id)
+        {
+           
+         
+           
+                var categories = _unitOfWork.Category.FindAll().ToList();
+                var viewModel = new CategoryViewModel
+                {
+                    ParentCategories = categories.Select(c => new SelectListItem
+                    {
+                        Value = c.CategoryId.ToString(),
+                        Text = c.CategoryName
+                    })
+                }; 
+             
+                return View(viewModel);
+   
+           
+
+        }
+        [HttpPost]
+       
+        public IActionResult Create(CategoryViewModel obh)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = new Category
+                {
+                    ParentCategoryId = obh.ParentCategoryId,
+                    CategoryName = obh.CategoryName,
+                    Description = obh.Description,
+                    CreatedAt = DateTime.Now,
+                    Slug = obh.Slug
+                };
+
+                _unitOfWork.Category.Create(category);
+                _unitOfWork.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            // در صورت خطا، مجدداً لیست دسته‌بندی‌های والد را ارسال کن
+            var categories = _unitOfWork.Category.FindAll().ToList();
+
+            obh.ParentCategories = categories.Select(c => new SelectListItem
+            {
+                Value = c.CategoryId.ToString(),
+                Text = c.CategoryName
+            });
+
+            return View(obh);
+        }
+
+
+
+
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,55 +11,20 @@ function dark() {
 
 
 
-// Modal
-$('#deleteModal').on('show.bs.modal', function (event) {
-    const button = $(event.relatedTarget); // دکمه‌ای که مدال را فعال کرده
-    const serviceId = button.attr('asp-route-id'); // دریافت ID از دکمه
-    const serviceName = button.data('name'); // دریافت نام خدمات
+$(document).ready(function () {
+    $("ul.a-collapse").click(function () {
+        // console.log($(this).hasClass("short"));
+        if ($(this).hasClass("short")) {
+            $(".a-collapse").addClass("short");
+            $(this).toggleClass("short");
+            $(".side-item-container").addClass("hide animated");
+            $("div.side-item-container", this).toggleClass("hide animated");
+        } else {
+            $(this).toggleClass("short");
+            $("div.side-item-container", this).toggleClass("hide animated");
+        }
 
-    // نمایش نام خدمات در مدال
-    $('#serviceName').text(serviceName);
 
-    // تنظیم لینک حذف در مدال با ID
-    $('#confirmDelete').attr('href', `/Services/Delete/${serviceId}`);
-});
-console.log(typeof bootstrap);
-
-
-//$(document).ready(function () {
-//    $('#servicesTable').DataTable({
-//        responsive: true, // فعال کردن ریسپانسیو
- 
-
-//        pageLength: 5, // تعداد رکوردها در هر صفحه
-
-//        language: {
-//            search: "جستجو:",
-//            lengthMenu: "نمایش _MENU_ رکورد در هر صفحه",
-
-//            zeroRecords: "هیچ رکوردی پیدا نشد",
-//            info: "نمایش _PAGE_ از _PAGES_",
-//            infoEmpty: "رکوردی موجود نیست",
-//            paginate: {
-//                first: "اولین",
-//                last: "آخرین",
-//                next: "بعدی",
-//                previous: "قبلی"
-//            }
-//        },
-
-//    });
-    //$('#servicesTable').DataTable({
-    //    ajax: '/api/services', // آدرس API
-    //    columns: [
-    //        { data: 'ServiceId' },
-    //        { data: 'ServiceName' },
-    //        { data: 'description' },
-    //        { data: 'Price' },
-    //        { data: 'Duration' },
-    //        { data: 'slug' }
-    //        ,
-    //    ]
-    //});
+    });
 
 });
